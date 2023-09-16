0d439f2ee6148e41c26e5e17b3403a94aba6ae68
BrianPizz <bpizzimentijr@outlook.com>
2023-09-16 19:28:08 -0400

    search bar will clear after search

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,7 +127,9 @@ function fiveDayforecast() {
                 forecastCard.append(forecastHumidity);
             }
         }
+        citySearchEl.val('')
     })
+    
 }
 
 function changeUnit() {
